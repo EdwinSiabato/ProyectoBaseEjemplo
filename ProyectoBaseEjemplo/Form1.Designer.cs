2ccc25e130a13a74ad16a8fce2bb7c8ec738c327
Edwin Siabato R <Edwin Siabato R@DESKTOP-D5PH2J8>
2021-10-04 11:23:42 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoBaseEjemplo/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿
+namespace ProyectoBaseEjemplo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtIdUsuario = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.txtIdentificacion = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btAgregar = new System.Windows.Forms.Button();
+            this.btModificar = new System.Windows.Forms.Button();
+            this.btEliminar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtAsunto = new System.Windows.Forms.TextBox();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grid
+            // 
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.grid.Location = new System.Drawing.Point(12, 406);
+            this.grid.Name = "grid";
+            this.grid.RowHeadersWidth = 62;
+            this.grid.RowTemplate.Height = 28;
+            this.grid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grid.Size = new System.Drawing.Size(797, 220);
+            this.grid.TabIndex = 0;
+            this.grid.Click += new System.EventHandler(this.grid_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IdUsuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Apellidos";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Identificacion";
+            // 
+            // txtIdUsuario
+            // 
+            this.txtIdUsuario.Location = new System.Drawing.Point(137, 26);
+            this.txtIdUsuario.Name = "txtIdUsuario";
+            this.txtIdUsuario.Size = new System.Drawing.Size(100, 26);
+            this.txtIdUsuario.TabIndex = 5;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(137, 99);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 26);
+            this.txtNombre.TabIndex = 6;
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Location = new System.Drawing.Point(137, 159);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(100, 26);
+            this.txtApellidos.TabIndex = 7;
+            // 
+            // txtIdentificacion
+            // 
+            this.txtIdentificacion.Location = new System.Drawing.Point(137, 214);
+            this.txtIdentificacion.Name = "txtIdentificacion";
+            this.txtIdentificacion.Size = new System.Drawing.Size(100, 26);
+            this.txtIdentificacion.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(375, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "CRUD";
+            // 
+            // btAgregar
+            // 
+            this.btAgregar.Location = new System.Drawing.Point(42, 330);
+            this.btAgregar.Name = "btAgregar";
+            this.btAgregar.Size = new System.Drawing.Size(113, 41);
+            this.btAgregar.TabIndex = 10;
+            this.btAgregar.Text = "AGREGAR";
+            this.btAgregar.UseVisualStyleBackColor = true;
+            this.btAgregar.Click += new System.EventHandler(this.btAgregar_Click);
+            // 
+            // btModificar
+            // 
+            this.btModificar.Location = new System.Drawing.Point(258, 329);
+            this.btModificar.Name = "btModificar";
+            this.btModificar.Size = new System.Drawing.Size(120, 42);
+            this.btModificar.TabIndex = 11;
+            this.btModificar.Text = "MODIFICAR";
+            this.btModificar.UseVisualStyleBackColor = true;
+            this.btModificar.Click += new System.EventHandler(this.btModificar_Click);
+            // 
+            // btEliminar
+            // 
+            this.btEliminar.Location = new System.Drawing.Point(482, 330);
+            this.btEliminar.Name = "btEliminar";
+            this.btEliminar.Size = new System.Drawing.Size(113, 41);
+            this.btEliminar.TabIndex = 12;
+            this.btEliminar.Text = "ELIMINAR";
+            this.btEliminar.UseVisualStyleBackColor = true;
+            this.btEliminar.Click += new System.EventHandler(this.btEliminar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(427, 105);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Telefono";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(427, 150);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 20);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Correo";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(427, 214);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 20);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Asunto";
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(523, 93);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(100, 26);
+            this.txtTelefono.TabIndex = 16;
+            // 
+            // txtAsunto
+            // 
+            this.txtAsunto.Location = new System.Drawing.Point(523, 208);
+            this.txtAsunto.Name = "txtAsunto";
+            this.txtAsunto.Size = new System.Drawing.Size(100, 26);
+            this.txtAsunto.TabIndex = 17;
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Location = new System.Drawing.Point(523, 156);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(100, 26);
+            this.txtCorreo.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 638);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.txtAsunto);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btEliminar);
+            this.Controls.Add(this.btModificar);
+            this.Controls.Add(this.btAgregar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtIdentificacion);
+            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtIdUsuario);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.grid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtIdUsuario;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.TextBox txtIdentificacion;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btAgregar;
+        private System.Windows.Forms.Button btModificar;
+        private System.Windows.Forms.Button btEliminar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtAsunto;
+        private System.Windows.Forms.TextBox txtCorreo;
+    }
+}
+
---
+++ ProyectoBaseEjemplo/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoBaseEjemplo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cargar();
+        }
+
+        private void cargar()
+        {
+            BaseEjemploDosEntitiesConnect contexto = new BaseEjemploDosEntitiesConnect();
+            grid.DataSource = contexto.Clientes.ToList();
+        }
+
+        private void llenar()
+        {
+            //Nos carga el registro según el que hayamos elegido. La idea es que cuando seleccione
+            //un registro se cargue en su respectiva celda.
+            this.txtIdUsuario.Text = grid.SelectedRows[0].Cells[0].Value.ToString();
+            this.txtNombre.Text = grid.SelectedRows[0].Cells[1].Value.ToString();
+            this.txtApellidos.Text = grid.SelectedRows[0].Cells[2].Value.ToString();
+            this.txtIdentificacion.Text = grid.SelectedRows[0].Cells[3].Value.ToString();
+            this.txtCorreo.Text = grid.SelectedRows[0].Cells[4].Value.ToString();
+            this.txtTelefono.Text = grid.SelectedRows[0].Cells[5].Value.ToString();
+            this.txtAsunto.Text = grid.SelectedRows[0].Cells[6].Value.ToString();
+        }
+
+        private void btAgregar_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(this.txtIdUsuario.Text);
+            string nombre = this.txtNombre.Text;
+            string apellidos = this.txtApellidos.Text;
+            string identificacion = this.txtIdentificacion.Text;
+            string correo = this.txtCorreo.Text;
+            string telefono = this.txtTelefono.Text;
+            string asunto = this.txtAsunto.Text;
+
+            using (BaseEjemploDosEntitiesConnect contexto = new BaseEjemploDosEntitiesConnect{ })
+            {
+                Clientes c = new Clientes
+                {
+                    IdUsuario = id,
+                    Nombre = nombre,
+                    Apellidos = apellidos,
+                    Identificacion = identificacion,
+                    Correo = correo,
+                    Telefono = telefono,
+                    Asunto = asunto
+
+                };
+                contexto.Clientes.Add(c);
+                contexto.SaveChanges();
+                cargar();
+            }
+
+        }
+
+        private void grid_Click(object sender, EventArgs e)
+        {
+            llenar();
+        }
+
+        private void btModificar_Click(object sender, EventArgs e)
+        {
+            using(BaseEjemploDosEntitiesConnect contexto = new BaseEjemploDosEntitiesConnect { })
+            {
+                //Seleccionamos un registro a modificar. 
+                int id = Convert.ToInt16(this.txtIdUsuario.Text);
+                string nombre = this.txtNombre.Text;
+                string apellidos = this.txtApellidos.Text;
+                string identificacion = this.txtIdentificacion.Text;
+                string correo = this.txtCorreo.Text;
+                string telefono = this.txtTelefono.Text;
+                string asunto = this.txtAsunto.Text;
+
+                Clientes c = contexto.Clientes.FirstOrDefault(x => x.IdUsuario == id);
+
+                c.IdUsuario = id;
+                c.Nombre = nombre;
+                c.Apellidos = apellidos;
+                c.Identificacion = identificacion;
+                c.Correo = correo;
+                c.Telefono = telefono;
+                c.Asunto = asunto;              
+
+                contexto.SaveChanges();
+                cargar();
+
+            }
+        }
+
+        private void btEliminar_Click(object sender, EventArgs e)
+        {
+            using (BaseEjemploDosEntitiesConnect contexto = new BaseEjemploDosEntitiesConnect { })
+            {
+             
+                int id = Convert.ToInt16(this.txtIdUsuario.Text);
+               
+                Clientes c = contexto.Clientes.FirstOrDefault(x => x.IdUsuario == id);
+
+                contexto.Clientes.Remove(c);
+                contexto.SaveChanges();
+                cargar();
+
+
+            }
+        }
+}
+}
